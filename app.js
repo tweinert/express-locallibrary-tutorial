20b9f7ce529363e9822ffec391177d853d7a67fd
Tait Weinert <tait.weinert@outlook.com>
2023-11-20 19:32:18 +1030

    added mongoose

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb+srv://admin:ebgdae@cluster0.glsdmhh.mongodb.net/?retryWrites=true&w=majority";
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
