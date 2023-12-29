a3390b5b83ca08f244f3561022c381f4e06bf527
Tait Weinert <tait.weinert@outlook.com>
2023-12-29 15:04:30 +1030

    ready for production

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require("dotenv").config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog");
+const compression = require("compression");
+const helmet = require("helmet");
 
 var app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://admin:ebgdae@cluster0.glsdmhh.mongodb.net/?retryWrites=true&w=majority";
+ 
+const mongoDB = process.env.DEV_MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
@@ -28,6 +40,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+app.use(limiter);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
